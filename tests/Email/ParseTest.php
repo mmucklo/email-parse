a6c4ce1dd6df36713170ad58c5c3970afe406dcd
Marius Krämer <marius@marius-kraemer.de>
2015-09-15 11:47:35 +0200

    Fix namespace in test

--- tests/Email/ParseTest.php
+++ tests/Email/ParseTest.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(__DIR__ . '/../../vendor/autoload.php');
-require_once(__DIR__ . '/../../src/Email/Parse.php');
+require_once(__DIR__ . '/../../src/Parse.php');
 
 use Email\Parse;
 
